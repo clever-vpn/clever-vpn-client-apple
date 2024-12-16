eb5cf36518b51196bf2ab266f08c908466bb2fa8
wubolin001 <wubolin001@outlook.com>
2024-12-16 15:48:16 +0800

    Initial Commit

---
+++ CleverVpnFramework/CleverVpnFramework.h
@@ -0,0 +1,18 @@
+//
+//  CleverVpnFramework.h
+//  CleverVpnFramework
+//
+//  Created by bolin wu on 2024/12/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CleverVpnFramework.
+FOUNDATION_EXPORT double CleverVpnFrameworkVersionNumber;
+
+//! Project version string for CleverVpnFramework.
+FOUNDATION_EXPORT const unsigned char CleverVpnFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleverVpnFramework/PublicHeader.h>
+
+
---
+++ CleverVpnFrameworkTests/CleverVpnFrameworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleverVpnFrameworkTests.swift
+//  CleverVpnFrameworkTests
+//
+//  Created by bolin wu on 2024/12/16.
+//
+
+import Testing
+@testable import CleverVpnFramework
+
+struct CleverVpnFrameworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
