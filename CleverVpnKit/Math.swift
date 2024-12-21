c3a16d8fad5e4dcd41cf4e37c45981b80c076b07
wubolin001 <wubolin001@outlook.com>
2024-12-21 11:19:24 +0800

    "init git"

---
+++ CleverVpnKit/Math.swift
@@ -0,0 +1,58 @@
+//
+//  Math.swift
+//  CleverVpnFramework
+//
+//  Created by bolin wu on 2024/12/16.
+//
+
+import Foundation
+
+
+internal import WireGuardKitGo
+
+public class SimpleAlgorithms {
+
+    public func fibonacci(_ n: Int) -> Int {
+        var a = 1
+        var b = 1
+        guard n > 1 else { return a }
+
+        (2...n).forEach { _ in
+            (a, b) = (a + b, a)
+        }
+        return a
+    }
+
+    public func factorial(n: Int) -> Int {
+        var result = 1
+        if n > 0 {
+            (1...n).forEach { i in
+                result *= i
+            }
+        }
+        return result
+    }
+    
+    public func getWgVersion() -> String {
+        return wgVersionEx()
+//        return "1.0.0"
+    }
+        
+        
+
+    public init() {
+        
+    }
+}
+
+
+
+
+func wgVersionEx() -> String {
+    if let versionCString = wgVersion() {
+        return String(cString: versionCString)
+    } else {
+        return "Unknown version"
+    }
+//    return "1.0.0"
+}
--- CleverVpnKitTests/CleverVpnFrameworkTests.swift
+++ CleverVpnKitTests/CleverVpnFrameworkTests.swift
@@ -12,6 +12,12 @@ struct CleverVpnFrameworkTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        let algorithms = SimpleAlgorithms()
+        let fibonacci = algorithms.fibonacci(10)
+        let factorial = algorithms.factorial(n: 10)
+        #expect(fibonacci == 89)
+        #expect(factorial == 3628800)
+        
     }
 
 }
